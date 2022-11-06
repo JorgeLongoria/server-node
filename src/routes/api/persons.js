0918e6761a2fef6f5fbbf860a7ca75d642948416
Jorge Longoria <jlongo48@stu.southtexascollege.edu>
2022-11-06 15:35:29 -0600

    Added basic model

--- src/routes/api/persons.js
+++ src/routes/api/persons.js
@@ -1,26 +1,41 @@
 /* eslint-disable prettier/prettier */
 import { Router } from 'express'
+import {
+  getPersons,
+  getPerson,
+  createPerson,
+  updatePerson,
+  deletePerson,
+} from '../../models/persons'
 
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.send({ msg: 'Get All Persons'})
+  const persons = getPersons()
+  res.send(persons)
 })
 
 router.get('/:id', (req, res) => {
-    res.send({ msg: `Getting Person ${req.params.id}`})
+  const person = getPerson(req.params.id)
+  res.send(person)
 })
 
 router.post('/', (req, res) => {
-  res.send({ msg: 'Creating a new Person'})
+  const newPerson = createPerson(req.body)
+  res.send(newPerson)
 })
 
 router.put('/:id', (req, res) => {
-  res.send({ msg: `Updating Person ${req.params.id}`})
+  const updatedPerson = updatePerson(req.params.id, req.body)
+  if (updatedPerson) {
+    res.send(updatedPerson)
+  }
 })
 
 router.delete('/:id', (req, res) => {
-  res.send({ msg: `Deleting Person ${req.params.id}`})
+  const deleted = deletePerson(req.params.id)
+  if (deleted) {
+  res.send({ msg: `Person ${req.params.id} Deleted` })
+  }
 })
-
 export default router
