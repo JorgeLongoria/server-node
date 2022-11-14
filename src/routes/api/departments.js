3828ff9daa912a3d150c085a8e24885bb2f71624
Jorge Longoria <jlongo48@stu.southtexascollege.edu>
2022-11-13 18:43:16 -0600

    Added departments endpoint

---
+++ src/routes/api/departments.js
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import { Router } from 'express'
+
+import { getDepartments, getDepartment } from '../../models/departments'
+
+const router = Router()
+
+router.get('/', async (req, res) => {
+    const departments = await getDepartments()
+    res.send(departments)
+})
+
+router.get('/:id', async (req, res) => {
+    const department = await getDepartment(req.params.id)
+    if (department) {
+      res.send(department)
+    } else {
+      res.status(404).send({ msg: 'Department not found' })
+    }
+})
+
+export default router
